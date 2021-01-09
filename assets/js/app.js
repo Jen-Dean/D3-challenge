1da3890cc5ac8a6eadb8362a807474e72f835b48
Jen-Dean <missjenniferdean@gmail.com>
2021-01-09 17:34:08 -0500

    setting up the D3

--- assets/js/app.js
+++ assets/js/app.js
@@ -38,7 +38,75 @@ var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Initial Chart
-
 var selectXAxis = "poverty"
 var selectYAxis = "obesity"
 
+function xScale(stateData, selectXAxis) {
+    
+    var xLinearScale = d3.scaleLinear()
+        .domain([d3.min(stateData, d => d[selectXAxis]),
+            d3.max(stateData, d => d[selectXAxis]) // * something???
+        ])
+        .range([0, width]);
+
+    return xLinearScale;
+}
+
+// Update the xAxis when you click on a different label
+function renderAxis(newXScale, xAxis) {
+    var bottomAxis = d3.axisBttom(newXScale);
+
+    xAxis.transition()
+        .duration(1000)
+        .call(bottomAxis);
+    
+    return xAxis;
+}
+// Create New Plot points based on the axis that you chose
+function renderPoints(pointsGroup, newXScale, selectXAxis) {
+
+    pointsGroup.transition()
+        .duration(1000)
+        .attr("cx", d => newXScale(d[selectXAxis]));
+    return pointsGroup;
+}
+
+// New Tooltip updates with new selections
+
+function updateToolTip(selectXAxis, pointsGroup) {
+
+    var tipLabel;
+
+    if (selectXAxis === "poverty") {
+        tipLabel = "Poverty Level:";
+    } else if (selectXAxis === "obesity") {
+        tipLabel = "Obesity Level:";
+    } else if (selectXAxis === "age") {
+        tipLabel = "Age:";
+    } else if (selectXAxis === "income") {
+        tipLabel = "Income:";
+    } else if (selectXAxis === "healthcare") {
+        tipLabel = "Healthcare:";
+    } else if (selectXAxis === "smokes") {
+        tipLabel = "Smoking:";
+    }
+
+    var toolTip = d3.tip()
+        .attr("class", "tooltip")
+        .offset([0,0])
+        .html(function(d) {
+            return (`${d.state}<br>${tipLabel} ${d[selectXAxis]}`);
+        });
+
+    pointsGroup.call(toolTip);
+
+    pointsGroup.on("mouseover", function(d) {
+        toolTip.show(d);
+    });
+
+    pointsGroup.on("mouseout", function(d) {
+        toolTip.hide(d)
+    })
+
+    return pointsGroup;
+}
